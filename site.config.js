498a706fd291715a0a28be1719d7f82a826093b0
pzjzeason <pzjzeason@qq.com>
2021-10-02 01:55:59 +0800

    fix: comment

--- site.config.js
+++ site.config.js
@@ -32,7 +32,7 @@ module.exports = {
   imageCDNHost: null,
 
   // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: 'https://github.com/pzjzeason/comment_base',
+  utterancesGitHubRepo: 'pzjzeason/comment_base',
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
